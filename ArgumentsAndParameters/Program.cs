c3249426e5e945ffaf3b96a56cc0b183576743a3
ValdoVentman <valdo.ventman@tktk.ee>
2023-04-15 10:33:00 +0300

    add week7functions code

---
+++ ArgumentsAndParameters/Program.cs
@@ -0,0 +1,17 @@
+﻿
+//PrintAnyWord();
+Console.WriteLine("Enter a word");
+string userInput = Console.ReadLine();
+
+PrintAnyWord(userInput); // userinput -argument
+
+
+static void PrintAnyWord(string anystring) //anystring -parameter
+{
+    anystring= anystring.ToUpper();
+   for(int i =0; i < 5; i++)
+    {
+        Console.WriteLine(anystring);
+    }
+}
+  
